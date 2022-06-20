8e8aab39dfc9961e03deefe3d85c350662570614
eldrouot <elisa.drouot@etu.uca.fr>
2022-06-20 16:47:16 +0200

    exercice termine

--- X-fenetre/Elisa/Elisa.c
+++ X-fenetre/Elisa/Elisa.c
@@ -52,11 +52,15 @@ int main()
         *window_1 = NULL;
     SDL_Window
         *window_2 = NULL;
+    SDL_Window
+        *window_3 = NULL;
 
     SDL_Renderer 
         *renderer1 = NULL;
     SDL_Renderer
         *renderer2 = NULL;
+    SDL_Renderer
+        *renderer3 = NULL;
 
     /* Initialisation de la SDL  + gestion de l'échec possible */
     if(SDL_Init(SDL_INIT_VIDEO) !=0 ){
@@ -80,7 +84,7 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    /* Création de la fenêtre deuxieme fenetre*/
+    /* Création de la deuxieme fenetre*/
     window_2  = SDL_CreateWindow(
         "Fenetre 2",
         0, 1000,
@@ -89,6 +93,21 @@ int main()
     
     
     if (window_2 == NULL){
+        SDL_Log("Error : SDL window 1 creation -%s\n",
+            SDL_GetError());
+        SDL_Quit();                              // On referme la SDL       
+        exit(EXIT_FAILURE);
+    }
+
+    /* Création de la troisieme fenetre*/
+    window_3  = SDL_CreateWindow(
+        "Fenetre 2",
+        1720, 0,
+        200, 200,
+        SDL_WINDOW_RESIZABLE);
+    
+    
+    if (window_3 == NULL){
         SDL_Log("Error : SDL window 1 creation -%s\n",
             SDL_GetError());
         SDL_Quit();                              // On referme la SDL       
@@ -119,11 +138,44 @@ int main()
 
     SDL_RenderPresent(renderer2);
 
+    /*creation 3eme renderer */
+    renderer3 = SDL_CreateRenderer(window_3, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer3 == 0) {
+     fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError()); 
+      /*faire ce qu'il faut pour quitter proprement */
+    }
+    
+    SDL_SetRenderDrawColor(renderer3, 255, 0, 0, 128);
+    SDL_RenderClear(renderer3);
+
+    SDL_RenderPresent(renderer3);
+
+    /*Mouvement fenetre 3*/
+    int i; int x; int y; int j;
+    while(j<10){
+        for(i=0;i<10;i++)
+        {
+            SDL_GetWindowPosition(window_3, &x, &y);
+            SDL_SetWindowPosition(window_3, x-60*i, y + 20*i);
+        }
+        for(i=0;i<10;i++)
+        {
+            SDL_GetWindowPosition(window_3, &x, &y);
+            SDL_SetWindowPosition(window_3, x+60*i, y-20*i);
+        }
+        j++;
+    }
+
+    SDL_SetWindowPosition(window_3, 500, 500);
+    SDL_SetWindowSize(window_3,400,400);
+
     SDL_Delay(5000); 
     SDL_DestroyRenderer(renderer1);  
-    SDL_DestroyRenderer(renderer2);  
-    SDL_DestroyWindow(window_1);
+    SDL_DestroyRenderer(renderer2);
+    SDL_DestroyRenderer(renderer3);  
+    SDL_DestroyWindow(window_1);  
     SDL_DestroyWindow(window_2);
+    SDL_DestroyWindow(window_3);
     
     SDL_Quit();   
     return 0; 
