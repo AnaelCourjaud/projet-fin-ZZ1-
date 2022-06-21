77a743c56cdc55a9f7112f46db3dc159ba513ecd
jedomanski <jeanne.domanski@etu.uca.com>
2022-06-21 20:55:53 +0200

    sauvegarder final

--- Jeu de la vie/Jeanne/sauvegarder.c
+++ Jeu de la vie/Jeanne/sauvegarder.c
@@ -1,19 +1,20 @@
 #include "sauvegarder.h"
 
 int grille[HAUTEUR][LARGEUR];
-for (i = 0; i < HAUTEUR; i++)
+/*for (i = 0; i < HAUTEUR; i++)
 {
     for (j = 0; j < LARGEUR; j++)
     {
         grille[i][j] = 0;
     }
 }
-grille[2][6] = 1;
+grille[2][6] = 1;*/
 
 void sauvegarder(int grille[HAUTEUR][LARGEUR])
 {
     FILE *fichier = NULL;
     time_t now;
+    time(&now);
     char nomfichier[100];
     strcpy(nomfichier, "sauvegarde");
     strcat(strcat(nomfichier, ctime(&now)), ".txt");
@@ -21,13 +22,11 @@ void sauvegarder(int grille[HAUTEUR][LARGEUR])
     int i, j;
     for (i = 0; i < HAUTEUR; i++)
     {
-        char ligne[LARGEUR + 1];
         for (j = 0; j < LARGEUR; j++)
         {
-            ligne[j] = grille[i][j] + '0';
+            fprintf(fichier, "%d ", grille[i][j]);
         }
-        ligne[LARGEUR] = 0;
-        fprintf(fichier, "%s\n", ligne);
+        fprintf(fichier, "\n");
     }
     fclose(fichier);
 }
@@ -38,30 +37,52 @@ int loadConfig(int grille[HAUTEUR][LARGEUR], char nom_fic[100])
 
     FILE *fichier = NULL;
     fichier = fopen(nom_fic, "r");
-    if (fichier == 0)
+    if (fichier == NULL)
         erreur = 0;
-    char ligne[LARGEUR];
-    int k;
-    for (k = 0; k < LARGEUR; k++)
-        ligne[k] = 4;
-    printf(ligne);
+    char ligne[LARGEUR+1];
+    // int k;
+    // for (k = 0; k < LARGEUR; k++)
+    //     ligne[k] = '4';
+    ligne[0] = '\0';
+    // printf(ligne);
+
     if (fichier)
     {
         int i = 0;
         int j;
-        while (fgets(ligne, LARGEUR, fichier))
+        printf("On commence ici\n");
+        // while (!feof(fichier))
+        // {
+        //     fgets(ligne, LARGEUR+1, fichier);
+
+        //     if(i<HAUTEUR)
+        //     {
+        //         for (j = 0; j < LARGEUR; j++)
+        //         {
+        //             if ((ligne[j] != '0') && (ligne[j] != '1'))
+        //                 erreur = 0;
+        //             grille[i][j] = ligne[j] - '0' ;
+        //             printf("%c", ligne[j]);
+        //         }
+        //         printf("\n");
+        //         i++;
+        //     }
+        // }
+        
+        while( !feof(fichier) )
         {
-            for (j = 0; j < LARGEUR; j++)
+             for (j = 0; j < LARGEUR; j++)
             {
-                if ((atoi(ligne) != 0) || (atoi(ligne) != 1) || (atoi(ligne) != 4))
-                    erreur = 0;
-                grille[i][j] = atoi(ligne[j]);
-                printf(ligne[j]);
+                fscanf(fichier, "%d", &grille[i][j]);
+                printf("%d ", grille[i][j]);
             }
+            printf("\n");
             i++;
         }
+        
         fclose(fichier);
     }
+
     return erreur;
 }
 
@@ -69,16 +90,17 @@ int main()
 {
     int grille[HAUTEUR][LARGEUR];
     int i,j;
-    /*for (i = 0; i < HAUTEUR; i++)
+    for (i = 0; i < HAUTEUR; i++)
     {
         for (j = 0; j < LARGEUR; j++)
         {
             grille[i][j] = 0;
         }
     }
-    grille[2][6] = 1;*/
+    grille[2][6] = 1;
 
-    int erreur = loadConfig(grille, "config.txt");
+    int erreur = loadConfig(grille, "config1.txt");
+    sauvegarder(grille);
     return 0;
 }
 // pour effacer un fichier : remove("nomFichier.xxx");
