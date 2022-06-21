4ee045720f9f85aa0a8a08c1b1dee0cea1b06847
eldrouot <elisa.drouot@etu.uca.fr>
2022-06-21 16:23:47 +0200

    ajouts jeu de la vie

--- Jeu de la vie/Elisa/jeuVie.h
+++ Jeu de la vie/Elisa/jeuVie.h
@@ -2,6 +2,7 @@
 #define JEUVIE_H
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
@@ -13,5 +14,6 @@
 #define LARGEUR 48
 
 void affichage(int grille[HAUTEUR][LARGEUR], SDL_Renderer *renderer);
+void creationTexte(char texte[], SDL_Renderer *renderer, int taille, int x, int y, int opacite);
 
 #endif
