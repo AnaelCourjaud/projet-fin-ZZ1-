9496337036751676fbe11ea15709765dbca68e32
Anael <courjaudanael@gmail.com>
2022-06-22 12:14:00 +0200

    version stable avec début d'interface

--- Jeu de la vie/Anael/module.c
+++ Jeu de la vie/Anael/module.c
@@ -1,9 +1,146 @@
 #include "module.h"
 
+void affichage(int grille[HAUTEUR][LARGEUR], SDL_Renderer *renderer)
+{
+    int i;
+    int j;
+    for (i = 0; i < HAUTEUR; i++)
+    {
+        for (j = 0; j < LARGEUR; j++)
+        {
+            
+
+    SDL_Rect rect = {40 * j + 1 + 15, 40 * i + 1 + 20, 38, 38};
+            if (grille[i][j] == 0)
+            {
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                SDL_RenderFillRect(renderer, &rect);
+            }
+            else
+            {
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+                SDL_RenderFillRect(renderer, &rect);
+            }
+        }
+    }
+    //SDL_RenderPresent(renderer);
+}
+
+int nb_voisins(int i, int j, int grille[HAUTEUR][LARGEUR], int monde)
+{
+    int voisins = 0;
+    int horz[3] = {-1, 0, 1};
+    int vert[3] = {-1, 0, 1};
+    int maxvert = 3;
+    int minvert = 0;
+    int minhorz = 0;
+    int maxhorz = 3;
+    if (monde == 0) // normal
+    {
+        if (j == 0)
+            minhorz = 1;
+        if (j == LARGEUR - 1)
+            maxhorz = 2;
+        if (i == 0)
+            minvert = 1;
+        if (i == HAUTEUR - 1)
+            maxvert = 2;
+    }
+    else // torique
+    {
+        if (j == 0)
+            horz[0] = LARGEUR - 1 - j;
+        if (j == LARGEUR - 1)
+            horz[2] = 0 - j;
+        if (i == 0)
+            vert[0] = HAUTEUR - 1 - i;
+        if (i == HAUTEUR - 1)
+            vert[2] = 0 - i;
+    }
+
+    for (int b = minvert; b < maxvert; b++)
+    {
+        for (int a = minhorz; a < maxhorz; a++)
+        {
+
+            if (horz[a] != 0 || vert[b] != 0)
+            {
+                if (grille[vert[b] + i][horz[a] + j] == 1)
+                    voisins++;
+            }
+        }
+    }
+    return voisins;
+}
+
+int nouvgen(int i, int j, int grille[HAUTEUR][LARGEUR], int nouvgrille[HAUTEUR][LARGEUR], int monde)
+{
+    int voisins = nb_voisins(i, j, grille, monde);
+    int modif = 0;
+    int survie[9] = {0, 1, 1, 1, 1, 1, 0, 0, 0};
+    int naissance[9] = {0, 0, 0, 1, 0, 0, 0, 0, 0};
+
+    if (grille[i][j] == 1)
+    {
+        if (survie[voisins] == 0)
+        {
+            nouvgrille[i][j] = 0;
+            modif++;
+        }
+        else
+        {
+            nouvgrille[i][j] = 1;
+        }
+    }
+    else
+    {
+        if (naissance[voisins] == 1)
+        {
+            nouvgrille[i][j] = 1;
+            modif++;
+        }
+        else
+        {
+            nouvgrille[i][j] = 0;
+        }
+    }
+    return modif;
+}
+
+int nouvgengrille(int grille[HAUTEUR][LARGEUR], int nouvgrille[HAUTEUR][LARGEUR], int monde)
+{
+    int nb_modif = 0;
+    for (int i = 0; i < HAUTEUR; i++)
+    {
+        for (int j = 0; j < LARGEUR; j++)
+            nb_modif = nb_modif + nouvgen(i, j, grille, nouvgrille, monde);
+    }
+    return nb_modif;
+}
+
+void initGrille(int grille[HAUTEUR][LARGEUR])
+{
+    for (int i = 0; i < HAUTEUR; i++)
+    {
+        for (int j = 0; j < LARGEUR; j++)
+        {
+            grille[i][j] = 0;
+        }
+    }
+    grille[6][6] = 1;
+    grille[6][5] = 1;
+    grille[7][5] = 1;
+    grille[8][5] = 1;
+    grille[7][4] = 1;
+    /*grille[2][0] = 1;
+    grille[5][4] = 1;
+    grille[5][3] = 1;
+    grille[0][3] = 1;*/
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void end_sdl(char ok,            // fin normale : ok = 0 ; anormale ok = 1
+void end_sdl(char ok,            // fin anormale : ok = 0 ; normale ok = 1
              char const *msg,    // message à afficher
              SDL_Window *window, // fenêtre à fermer
              SDL_Renderer *renderer)
@@ -48,3 +185,31 @@ int chargerConfigInit(int grille[HAUTEUR][LARGEUR], int argc, char nomFic[])
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void creationTexte(char texte[], SDL_Renderer *renderer, int taille, int x, int y, int opacite)
+{
+
+    TTF_Font *font = NULL;                   // la variable 'police de caractère'
+    font = TTF_OpenFont("font.ttf", taille); // La police à charger, la taille désirée
+    if (font == NULL)
+        fprintf(stderr, "Can't load font  %s\n", SDL_GetError());
+
+    TTF_SetFontStyle(font, TTF_STYLE_ITALIC | TTF_STYLE_BOLD); // en italique, gras
+
+    SDL_Color color = {0, 0, 0, opacite};
+    SDL_Surface *text_surface = NULL;                          // la surface  (uniquement transitoire)
+    text_surface = TTF_RenderText_Blended(font, texte, color); // création du texte dans la surface
+    if (text_surface == NULL)
+        fprintf(stderr, "Can't create text surface %s\n", SDL_GetError());
+
+    SDL_Texture *text_texture = NULL;                                    // la texture qui contient le texte
+    text_texture = SDL_CreateTextureFromSurface(renderer, text_surface); // transfert de la surface à la texture
+    if (text_texture == NULL)
+        fprintf(stderr, "Can't create texture from surface %s\n", SDL_GetError());
+    SDL_FreeSurface(text_surface); // la texture ne sert plus à rien
+
+    SDL_Rect pos = {x, y, 1000, 1000};                          // rectangle où le texte va être prositionné
+    SDL_QueryTexture(text_texture, NULL, NULL, &pos.w, &pos.h); // récupération de la taille (w, h) du texte
+    SDL_RenderCopy(renderer, text_texture, NULL, &pos);
+    SDL_DestroyTexture(text_texture);
+}
--- Jeu de la vie/Anael/module.h
+++ Jeu de la vie/Anael/module.h
@@ -2,6 +2,7 @@
 #define MODULE_H
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
@@ -16,12 +17,21 @@
 #define STABLE 60
 #define FIN 70
 
-#define HAUTEUR 25
-#define LARGEUR 48
+#define HAUTEUR 24
+#define LARGEUR 40
 
 #define TAILLE_MAX 50
 
+
+
+
 void end_sdl(char, char const *, SDL_Window *, SDL_Renderer *);
 int chargerConfigInit(int[HAUTEUR][LARGEUR], int, char *);
+void creationTexte(char texte[], SDL_Renderer *renderer, int taille, int x, int y, int opacite);
+void initGrille(int grille[HAUTEUR][LARGEUR]);
+int nouvgengrille(int grille[HAUTEUR][LARGEUR], int nouvgrille[HAUTEUR][LARGEUR], int );
+int nouvgen(int, int, int grille[HAUTEUR][LARGEUR], int nouvgrille[HAUTEUR][LARGEUR], int );
+int nb_voisins(int , int , int grille[HAUTEUR][LARGEUR], int );
+void affichage(int grille[HAUTEUR][LARGEUR], SDL_Renderer *);
 
 #endif
