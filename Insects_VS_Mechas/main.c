b4a792a3dae6e983c973a63eb0bf64106299e6b6
Anael <courjaudanael@gmail.com>
2022-06-27 18:36:52 +0200

    insertion squelette

--- Insects_VS_Mechas/main.c
+++ Insects_VS_Mechas/main.c
@@ -100,239 +100,105 @@ SDL_RenderCopy(renderer, fond.textureSprite, &fond.source, &fond.destination);
     printf("après délai\n");
 SDL_DestroyTexture(textureSprite);
 
-    /*
-        SDL_bool program_on = SDL_TRUE; // Booléen pour dire que le programme doit continuer
-        SDL_Event event;                // c'est le type IMPORTANT !!
+SDL_bool program_on = SDL_TRUE; // Booléen pour dire que le programme doit continuer
+    SDL_Event event;                // c'est le type IMPORTANT !!
 
-        SDL_Texture *sprite1;
-        SDL_Texture *sprite2;
-        SDL_Texture *textureFond;
+    SDL_Texture *sprite1;
+    SDL_Texture *sprite2;
+    SDL_Texture *textureFond;
 
-        int nbrImageSprite1;
-        int nbrImageSprite2;
-        int animationFinieTerminee = 1;
-        int gameOver = 0;
+    int nbrImageSprite1;
+    int nbrImageSprite2;
+    int animationFinieTerminee = 1;
+    int gameOver = 0;
 
-        int ETATJEU = ACCUEIL;
-        int i = 0;
-        int j=0;
+    int ETATJEU = ACCUEIL;
+    int i = 0;
+    int j=0;
 
-        int PV[2] = {10, 10};
-        int etat[2] = {EAU, EAU};
+    int PV[2] = {10, 10};
+    int etat[2] = {EAU, EAU};
 
-        while (program_on)
-        {
-            // Voilà la boucle des évènements
-            int interessant = 0;
-            int choixFait = 0;
-            int etatPrec; //sert à l'utilisation de la chaîne de Markov
+    while (program_on)
+    {
+        // Voilà la boucle des évènements
+        int interessant = 0;
+        int choixFait = 0;
+        int etatPrec; //sert à l'utilisation de la chaîne de Markov
 
-        // *************************  Gestion des evenements *****************************
-        // *                                                                             *
-        // *******************************************************************************
+    /*************************  Gestion des evenements *****************************/
+    /*                                                                             */
+    /*******************************************************************************/
 
-            while ((interessant == 0) && (SDL_PollEvent(&event)))
-            {   // tant que la file d'évènements n'est pas vide : défiler l'élément en tête et l'on a pas d'évènements interessants à traiter
-                // de file dans 'event'
-                switch (event.type)
-                { // En fonction de la valeur du type de cet évènement
-                case SDL_QUIT:
-                    interessant = 1;        // Un évènement simple, on a cliqué sur la x de la fenêtre
-                    program_on = SDL_FALSE; // Il est temps d'arrêter le programme
+        while ((interessant == 0) && (SDL_PollEvent(&event)))
+        {   // tant que la file d'évènements n'est pas vide : défiler l'élément en tête et l'on a pas d'évènements interessants à traiter
+            // de file dans 'event'
+            switch (event.type)
+            { // En fonction de la valeur du type de cet évènement
+            case SDL_QUIT:
+                interessant = 1;        // Un évènement simple, on a cliqué sur la x de la fenêtre
+                program_on = SDL_FALSE; // Il est temps d'arrêter le programme
+                break;
+            case SDL_KEYUP: // Le type de event est : une touche lachée
+
+                switch (event.key.keysym.sym)
+                { // la touche appuyée est ...
+                case SDLK_SPACE:
+
+                    interessant = 1;
                     break;
-                case SDL_KEYUP: // Le type de event est : une touche lachée
+                case SDLK_q:
 
-                    switch (event.key.keysym.sym)
-                    { // la touche appuyée est ...
-                    case SDLK_SPACE:
-                        if (ETATJEU == ACCUEIL)
-                        {
-                            ETATJEU = JEU;
-                            i=0;
-                            j=0;
-                            animationFinieTerminee = 1;
-                            gameOver = 0;
-                            PV[0] = 10;
-                            PV[1] = 10;
-                            etat[0] = EAU;
-                            etat[1] = EAU;
-                        }
                         interessant = 1;
-                        break;
-                    case SDLK_q:
-                        if(animationFinieTerminee ==1){
-                            if (ETATJEU == JEU)
-                            {
-                                ETATJEU = ACCUEIL;
-                            }
-                            interessant = 1;
-                        }
-                        break;
-                    case SDLK_e:
-                        etat[0] = 0;
-                        choixFait = 1;
-                        interessant = 1;
-                        break;
-                    case SDLK_f:
-                        etat[0] = 1;
-                        choixFait = 1;
-                        interessant = 1;
-                        break;
-                    case SDLK_t:
-                        etat[0] = 2;
-                        choixFait = 1
-                           255);;
-                        interessant = 1;
-                        break;
-                    case SDLK_d:
-                        etatPrec = etat[0];
-                        etat[0] = 3;
-                        choixFait = 1;
-                        interessant = 1;
-                        break;
-                    default:
-                        break;
-                    }
+                    
+                    break;
+                case SDLK_e:
+           
+                    interessant = 1;
+                    break;
+                case SDLK_f:
+   
+                    interessant = 1;
+                    break;
+                case SDLK_t:
+   
+                    interessant = 1;
+                    break;
+                case SDLK_d:
 
-                default: // L'évènement défilé ne nous intéresse pas
+                    interessant = 1;
+                    break;
+                default:
                     break;
                 }
-            }
-            interessant = 0;
 
-            switch (ETATJEU)
-            {
-            case ERREUR:
-                break;
-            case ACCUEIL:
-
-                SDL_RenderClear(renderer);
-                creationImage(texture[0][8],window, renderer, tailleFenetre.w, tailleFenetre.h, 0, 0); //on affiche le fond inanimé
-
-                creationTexte("Bienvenue :", "normal", "./fonts/font.ttf", renderer, 100, 600, 100, 255);
-                creationTexte("Commandes :", "surligne", "./fonts/font2.ttf", renderer, 90, 400, 300, 255);
-                creationTexte("E pour attaque eau", "normal", "./fonts/font2.ttf", renderer, 90, 500, 450, 255);
-                creationTexte("F pour attaque feu", "normal", "./fonts/font2.ttf", renderer, 90, 500, 550, 255);
-                creationTexte("T pour attaque terre", "normal", "./fonts/font2.ttf", renderer, 90, 500, 650, 255);
-                creationTexte("D pour defense", "normal", "./fonts/font2.ttf", renderer, 90, 500, 750, 255);
-                creationTexte("Q pour revenir ici", "normal", "./fonts/font2.ttf", renderer, 90, 500, 850, 255);
-
-                break;
-            case JEU:
-
-                if(choixFait == 1 && animationFinieTerminee == 1 && gameOver == 0){ // quand l'utilisateur a appuyé sur une touche, qu'il n'y a pas d'animation finie en cours et que le jeu n'est pas fini, on traite le jeu en fonction de la touche pressée
-                    changementEtatEnnemi(etat);
-                    reussitedefense(etat, etatPrec);
-                    int resultat = gagnant(etat);
-                    resolutionPV(PV, resultat, etat);
-                    sprite1 = textureAttaque(texture,0, etat, resultat);
-                    sprite2 = textureAttaque(texture,1, etat, resultat);
-
-                    choixFait = 0;
-                    animationFinieTerminee = 0;
-                    i=0;
-                }
-
-                if(i >= nbrImageSpriteFini && animationFinieTerminee == 0){ // quand une animation finie a affiché chacunes de ses 10 images, on l'arrete
-                    animationFinieTerminee = 1;
-                    i=0;
-                }
-
-                if(animationFinieTerminee == 1){ // quand une animation finie est terminée, on revient à l'état de base : les deux persos qui donnent des coups de poing dans le vide (animation infinie)
-                    sprite1 = texture[0][5];
-                    sprite2 = texture[1][5];
-
-                }
-
-                textureFond = texture[1][8]; //par défaut
-                int nbrImageFond = 1; // possibilité de fond animé
-
-                if(((PV[0] <= 0)||(PV[1] <= 0))&&(animationFinieTerminee == 1 )){ // si un des deux joueurs est mort et qu'il n'y a pas d'animation finie en cours
-                    if(PV[1] <=0 ){
-                    sprite1 = texture[0][6];
-                    sprite2 = texture[1][7];
-                    textureFond = texture[0][9];
-                }
-                if(PV[0] <=0 ){
-                    sprite1 = texture[0][7];
-                    sprite2 = texture[1][6];
-                    textureFond = texture[1][9];
-                }
-                    gameOver = 1;
-                }
-
-                for(int i=0;i<ligneTexture;i++){ // En quelque sorte un "tableau du nombre d'image en fonction du sprite" (on a pas eu le temps de faire ça "proprement" avec un vrai tableau).
-                    for(int j=0;j<colonneTexture;j++){
-                        if(sprite1 == texture[i][j]){
-                            if(j==0 || j==1 || j==2 || j==3 || j==4){
-                                nbrImageSprite1=10;
-                            }else if(j==5 || j==6){
-                                nbrImageSprite1=2;
-                            }else if(j == 7){
-                                nbrImageSprite1=3;
-                            }else if(j == 8){
-                                if(i==0){
-                                    nbrImageSprite1 = 1;
-                                }else if(i==1){
-                                        nbrImageSprite1 =1;
-                                }
-                            }
-                        }
-                        if(sprite2 == texture[i][j]){
-                            if(j==0 || j==1 || j==2 || j==3 || j==4){
-                                nbrImageSprite2=10;
-                            }else if(j==5 || j==6){
-                                nbrImageSprite2=2;
-                            }else if(j == 7){
-                                nbrImageSprite2=3;
-                            }else if(j == 8){
-                                if(i==0){
-                                    nbrImageSprite2 = 1;
-                                }else if(i==1){
-                                        nbrImageSprite2 =1;
-                                }
-                            }
-                        }
-                        if(textureFond == texture[i][j]){
-                            if(j==0 || j==1 || j==2 || j==3 || j==4){
-                                nbrImageFond=10;
-                            }else if(j==5 || j==6){
-                                nbrImageFond=2;
-                            }else if(j == 7){
-                                nbrImageFond=3;
-                            }else if(j == 8){
-                                if(i==0){
-                                    nbrImageFond = 1;
-                                }else if(i==1){
-                                        nbrImageFond =1;
-                                }
-                            }
-                        }
-                   }
-                }
-
-                Animation(sprite1, sprite2, textureFond, renderer, window, 400, 400, 200, 1300, 500, nbrImageSprite1,nbrImageSprite2, nbrImageFond, i, j);
-
-                i++;
-                j++;
-
-                char message[50];
-                char message2[50];
-                sprintf(message,"PV : %d",PV[0]);
-                sprintf(message2,"PV : %d",PV[1]);
-                creationTexte(message, "normal", "./fonts/font.ttf", renderer, 100, 10, 350, 255);
-                creationTexte(message2, "normal", "./fonts/font.ttf", renderer, 100, 1450, 350, 255);
-
-                break;
-            default:
-                ETATJEU = ERREUR;
+            default: // L'évènement défilé ne nous intéresse pas
                 break;
             }
-
-            SDL_RenderPresent(renderer);
-            SDL_Delay(150);
         }
-    */
+        interessant = 0;
+
+        switch (ETATJEU)
+        {
+        case ERREUR:
+            break;
+        case ACCUEIL:
+
+
+            break;
+        case JEU:
+
+           
+            break;
+        default:
+            ETATJEU = ERREUR;
+            break;
+        }
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(150);
+    }
+
     end_sdl(1, "FIN NORMALE", window, renderer);
     return 0;
 }
